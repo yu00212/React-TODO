ae9b19fa19341947bd656697521215bf833553b1
ゆう <yu2000212@icloud.com>
2023-02-04 04:41:24 +0000

    Stateで表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return <div></div>;
-}
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(['ああああ', 'いいいい']);
+  const [completeTodos, setCompleteTodos] = useState(['うううう'])
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div className="incomplate-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="complate-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map(() => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
