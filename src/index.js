4f28950841dd67d951cba596034301fc6471e542
ゆう <yu2000212@icloud.com>
2023-02-04 02:30:52 +0000

    初期準備

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import { App } from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
